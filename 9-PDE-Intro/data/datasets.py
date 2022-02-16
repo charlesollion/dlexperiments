96d6bd4726d75f35e26b69dff80c88d7e5442054
charlesollion <charles.ollion@gmail.com>
2022-02-16 18:58:04 +0100

    split notebooks into 2, first degenerate version for kpp

--- 9-PDE-Intro/data/datasets.py
+++ 9-PDE-Intro/data/datasets.py
@@ -14,18 +14,18 @@ class BurgersDataset():
         self.curr_idx = 0
         self.curr_f_idx = 0
         self.u0 = u0
-        self.generate_data(key, N_u)
+        self.generate_data(key)
 
 
-    def generate_data(self, key, N_u):
+    def generate_data(self, key):
         # Generate border data
         key, subkey = random.split(key)
-        data_type = random.uniform(key, (N_u,))>0.5
+        data_type = random.uniform(key, (self.N_u, 1))>0.5
         key, subkey1, subkey2, subkey3 = random.split(key, 4)
-        self.x_data = data_type * (random.uniform(subkey1, (N_u,))*2.0-1.0)+ \
-            (1-data_type) * ((random.uniform(subkey2, (N_u,))>0.5)*2.0-1.0)
-        self.t_data = data_type * 0 + ((1-data_type) * random.uniform(subkey3, (N_u,)))
-        self.u_data = data_type * self.u0(self.x_data)
+        self.x_data = data_type * (random.uniform(subkey1, (self.N_u, 1))*2.0-1.0)+ \
+            (1-data_type) * ((random.uniform(subkey2, (self.N_u, 1))>0.5)*2.0-1.0)
+        self.t_data = data_type * 0 + ((1-data_type) * random.uniform(subkey3, (self.N_u, 1)))
+        self.u_data = data_type * np.expand_dims(self.u0(self.x_data), -1)
         # Generate inside data
         key1, key2 = random.split(key, 2)
         self.t_f_data = random.uniform(key1, (self.N_f, 1))
@@ -40,9 +40,9 @@ class BurgersDataset():
             self.curr_idx = 0
         else:
             self.curr_idx = self.curr_idx + 1
-        x_ = np.expand_dims(self.x_data[bstart:bend], -1)
-        t_ = np.expand_dims(self.t_data[bstart:bend], -1)
-        u_ = np.expand_dims(self.u_data[bstart:bend], -1)
+        x_ = self.x_data[bstart:bend]
+        t_ = self.t_data[bstart:bend]
+        u_ = self.u_data[bstart:bend]
         return t_, x_, u_
 
 
@@ -58,3 +58,29 @@ class BurgersDataset():
         x_b = self.x_f_data[bstart:bend]
 
         return t_b, x_b
+
+
+class KPPDataset(BurgersDataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+    def generate_data(self, key):
+        # Generate border data
+        key, subkey = random.split(key)
+        #data_type = random.uniform(key, (self.N_u, 1))>0.5
+        data_type = np.ones((self.N_u, 1))
+        key, subkey1, subkey2, subkey3, subkey4, subkey5 = random.split(key, 6)
+        x_data_t0 = data_type * (random.uniform(subkey1, (self.N_u, 2)))
+        border_dim = random.uniform(subkey2, (self.N_u, 1))>0.5
+        border_sign = random.uniform(subkey4, (self.N_u, 1))>0.5
+        border_value = random.uniform(subkey5, (self.N_u, 1))
+        x_data_borderx = (1-data_type) * border_dim * np.concatenate((border_sign, border_value), axis=-1)
+        x_data_bordery = (1-data_type) * (1-border_dim) * np.concatenate((border_value, border_sign), axis=-1)
+        self.x_data = x_data_t0 + x_data_borderx + x_data_bordery
+        self.t_data = data_type * 0 + ((1-data_type) * random.uniform(subkey3, (self.N_u, 1)))
+        self.u_data = data_type * np.expand_dims(self.u0(self.x_data), -1)
+        # Generate inside data
+        key1, key2 = random.split(key, 2)
+        self.t_f_data = random.uniform(key1, (self.N_f, 1))
+        self.x_f_data = random.uniform(key2, (self.N_f, 2))
--- 9-PDE-Intro/data/display.py
+++ 9-PDE-Intro/data/display.py
@@ -30,3 +30,27 @@ def display_burgers_slice(batched_u, resolution=30, slices=[0.25, 0.5, 0.75]):
         values = batched_u(t, x)
         plt.plot(values)
         plt.ylim((-1, 1))
+
+
+def display_KPP_at_times(batched_u, resolution=30, times=[0.0,]):
+    num = len(times)
+    plt.figure(figsize=(10,6))
+    for i in range(num):
+        plt.subplot(1, num,i+1)
+        tt = np.ones((resolution, 1)) * times[i]
+        xx = np.expand_dims(np.linspace(0,1, resolution),-1)
+        yy = np.expand_dims(np.linspace(0,1, resolution),-1)
+        map_out=[]
+
+        for ix in range(resolution):
+            xrow = np.expand_dims(np.repeat(xx[ix], resolution),axis=-1)
+            row = np.concatenate((xrow,yy), axis=-1)
+            outp = batched_u(tt, row)
+            #outp = u0(row)
+            map_out.append(np.expand_dims(outp, axis=-1))
+        map_out = np.hstack(map_out)
+        plt.imshow(map_out, vmin=-0.05, vmax=1.0)
+        plt.xticks([x*resolution/5 for x in range(5)], [round(i/5.0, 2) for i in range(5)])
+        plt.yticks([x*resolution/5 for x in range(5)], [round(i/5.0, 2) for i in range(5)])
+
+    plt.colorbar()
