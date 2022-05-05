3d9692141a0ec3f5cbd18bf17a35b7dfa3bab315
charlesollion <charles.ollion@gmail.com>
2022-05-05 15:23:09 +0200

    ajout d'un meilleur net

--- 9-PDE-Intro/models/nets.py
+++ 9-PDE-Intro/models/nets.py
@@ -12,7 +12,34 @@ class MLP(nn.Module):
     features: Sequence[int]
 
     def setup(self):
+        self.layers_m1 = [nn.Dense(feat) for feat in self.features]
+        self.layers_m2 = [nn.Dense(feat) for feat in self.features]
+
+    def __call__(self, t, x):
+        input = np.concatenate((t,x))
+        final_output = 0 # Initialisation
+        for i, (layer1, layer2) in enumerate(zip(self.layers_m1, self.layers_m2)):
+            x1 = layer1(x)
+            x2 = layer2(x)
+            output = (1. - t) * x1 + t * x2
+            if i != len(self.layers_m1) - 1:
+                input = nn.relu(output)
+            else:
+                final_output = output
+        return final_output
+
+class UNet(nn.Module):
+    features: Sequence[int]
+
+    def setup(self):
         self.layers = [nn.Dense(feat) for feat in self.features]
+        self.layer_m1 = [nn.Dense(feat) for feat in self.features]
+        self.layer_m2 = [nn.Dense(feat) for feat in self.features]
+
+    def mix(self, t, x):
+        x1 = self.layer_m1(x)
+        x2 = self.layer_m2(x)
+        return (1. - t) * x1 + t * x2
 
     def __call__(self, t, x):
         input = np.concatenate((t,x))
